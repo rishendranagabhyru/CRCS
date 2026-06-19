eb2fd54ea65c356d273ce3e6bbae16c8c0ecd372
rishendranagabhyru <rishendranagabhyru@gmail.com>
2026-06-19 16:25:51 +0530

    Day 7 and 8

---
+++ Day7_skill/my-app/src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+
+import { ThemeProvider } from "./components/ThemeContext";
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Navbar />
+
+      <div className="layout">
+        <Sidebar />
+
+        <main>
+          <Home />
+        </main>
+      </div>
+
+      <Footer />
+    </ThemeProvider>
+  );
+}
+
+export default App;
